7f6c1aa4307f4dfed89c2f262282435c1f4dfe41
Ryan Bradley <ryanbradley315@gmail.com>
2023-08-09 18:31:19 -0500

    Add further sections to homepage. Fix button styling.

--- client/src/routes/home/Home.tsx
+++ client/src/routes/home/Home.tsx
@@ -1,4 +1,6 @@
 import HeroMobile from '../../assets/mobile-images/create-and-share.jpg'
+import BeautifulStoriesMobile from '../../assets/mobile-images/beautiful-stories.jpg'
+import EveryoneMobile from '../../assets/mobile-images/designed-for-everyone.jpg'
 import Button from '../../components/button/Button'
 import styles from './home.module.css'
 
@@ -28,6 +30,62 @@ export default function Home() {
                         variant='arrow-white'
                     >
                         GET AN INVITE
+                    </Button>
+
+                </div>
+
+            </section>
+
+            <section
+                className={styles.stories}
+            >
+
+                <img
+                    srcSet={`${BeautifulStoriesMobile}`}
+                />
+
+                <div>
+
+                    <h2>
+                        BEAUTIFUL STORIES EVERY TIME
+                    </h2>
+
+                    <p>
+                        We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone.
+                    </p>
+
+                    <Button
+                        variant='arrow-black'
+                    >
+                        VIEW THE STORIES
+                    </Button>
+
+                </div>
+
+            </section>
+
+            <section
+                className={styles.stories}
+            >
+
+                <img
+                    srcSet={`${EveryoneMobile}`}
+                />
+
+                <div>
+
+                    <h2>
+                        DESIGNED FOR EVERYONE
+                    </h2>
+
+                    <p>
+                        Photosnap can help you create stories that resonate with your audience. Our tool is designed for photographers of all levels, brands, businesses, you name it.
+                    </p>
+
+                    <Button
+                        variant='arrow-black'
+                    >
+                        VIEW THE STORIES
                     </Button>
 
                 </div>
