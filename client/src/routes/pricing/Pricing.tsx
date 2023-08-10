5d018f14e380bdcd060eec3eb32fc876f1c7a299
Ryan Bradley <ryanbradley315@gmail.com>
2023-08-10 11:01:11 -0500

    Add hero to pricing route.

--- client/src/routes/pricing/Pricing.tsx
+++ client/src/routes/pricing/Pricing.tsx
@@ -1,10 +1,30 @@
-
+import Hero from "../../components/hero/Hero"
+import PricingHero from '../../assets/mobile-images/pricing.jpg'
 
 export default function Pricing() {
     return (
         <>
         
-            Pricing
+            <Hero>
+
+                <img
+                    src={PricingHero}
+                    alt="Pricing"
+                />
+
+                <div>
+
+                    <h1>
+                        PRICING
+                    </h1>
+
+                    <p>
+                        Create your stories, Photosnap is a platform for photographers and visual storytellers. It&apos;s the simple way to create and share your photos.
+                    </p>
+
+                </div>
+
+            </Hero>
         
         </>
     )
