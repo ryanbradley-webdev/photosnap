2dbc56a4dca40c31f33d6039ac58b8615e63a1dd
Ryan Bradley <ryanbradley315@gmail.com>
2023-08-10 10:58:31 -0500

    Add hero component with image to features route.

--- client/src/routes/features/Features.tsx
+++ client/src/routes/features/Features.tsx
@@ -1,10 +1,30 @@
-
+import Hero from "../../components/hero/Hero";
+import FeaturesHero from '../../assets/mobile-images/features.jpg'
 
 export default function Features() {
     return (
         <>
         
-            Features
+            <Hero>
+
+                <img
+                    src={FeaturesHero}
+                    alt="Features"
+                />
+
+                <div>
+
+                    <h1>
+                        FEATURES
+                    </h1>
+
+                    <p>
+                        We make sure all of our features are designed to be loved by every aspiring and even professional photographers who want to share their stories.
+                    </p>
+
+                </div>
+
+            </Hero>
             
         </>
     )
